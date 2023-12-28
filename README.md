ee159a1c19f84251062f8af8aea54d030d8d962f
DaneJacobson <danej@princeton.edu>
2023-12-28 04:20:00 -0500

    breaking down dataset

---
+++ README.md
@@ -0,0 +1,9 @@
+Data Extraction, download dataset then run extractor.py
+
+TODO:
+- Right now the dataset has 226 countries (including a \N)
+- Swap column names with real HS4 classifications
+
+Research Notes
+- 210 -> 302 achieves 100% at max depth of 30
+- 210 -> 5906 achieves 100% at max depth of 29!!!
--- extractor.py
+++ extractor.py
@@ -1,32 +1,12 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-import time
+import pandas as pd
 
-import const
+file_path = './data/trade_i_baci_a_92.tsv'
+chunk_size = 10000
+chunks_2021 = []
 
-# Specify the path to the chromedriver.exe
-webdriver_service = Service(const.DRIVER_PATH)
+for chunk in pd.read_csv(file_path, delimiter='\t', chunksize=chunk_size):
+    chunks_2021.append(chunk[chunk['year'] == 2021])
 
-# Set up options for the driver (like headless mode)
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')  # Runs Chrome in headless mode.
-
-# Set up the driver
-driver = webdriver.Chrome(service=webdriver_service, options=options)
-
-# The URL you want to open
-url = "https://oec.world/api/olap-proxy/data?cube=trade_i_baci_a_92&drilldowns=Year,HS4&measures=Trade%20Value&parents=true&Year=2021&Exporter%20Country=nausa"
-
-# Open the URL
-driver.get(url)
-
-# Wait for the page to load
-time.sleep(5)  # Adjust this according to your needs
-
-# Now, you can access the HTML content of the page
-page_source = driver.page_source
-print(page_source)  # This will print the HTML source code of the page
-
-# Close the browser
-driver.quit()
+data_2021 = pd.concat(chunks_2021)
+data_2021.reset_index()
+data_2021.to_csv('./data/hs92-2021.csv', index=False)
